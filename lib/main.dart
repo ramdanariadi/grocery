f2ce71cb5e356a54a45ea1b88c9aacd3ccca18a5
ramdan <ramadanariadi@gmail.com>
2021-11-20 11:47:36 +0800

    update pubspect

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,7 @@ class MyApp extends StatelessWidget {
         }
 
         if (setting.name == MyAccount.routeName) {
+          
           return CustomPageRoute(child: MyAccount());
         }
 
