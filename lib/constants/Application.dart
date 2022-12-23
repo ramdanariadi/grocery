04e98198b35ede1f033900e8e7c263fce3b51234
Ramdan Ariadi <ramadanariadi@gmail.com>
2022-12-24 01:24:43 +0800

    update Application.dart

--- lib/constants/Application.dart
+++ lib/constants/Application.dart
@@ -1,4 +1,4 @@
 class Application {
   static final double defaultPadding = 20.0;
-  static final String httBaseUrl = 'https://tunas-grocery.masuk.web.id';
+  static final String httBaseUrl = '';
 }
