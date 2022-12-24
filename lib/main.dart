3f464031a4b0fb9e230fb98fab0d407458ad457b
Ramdan Ariadi <ramadanariadi@gmail.com>
2022-12-24 21:01:55 +0800

    update application sheel route

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:go_router/go_router.dart';
 import 'package:grocery/chart/Cart.dart';
 import 'package:grocery/custom_widget/ScaffoldBottomActionBar.dart';
 import 'package:grocery/home/Home.dart';
+import 'package:grocery/product/ProductCard.dart';
+import 'package:grocery/product/ProductDetail.dart';
+import 'package:grocery/products/ProductGroupGridItems.dart';
 import 'package:grocery/products/Products.dart';
 import 'package:grocery/profile/MyAccount.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -33,9 +36,24 @@ class MyApp extends StatelessWidget {
         ShellRoute(
           builder: (context, state, child) => ScaffoldBottomActionBar(child: child,),
           routes: [
+            /* Home route **/
             GoRoute(path: Home.routeName, builder: (context, state) => Home()),
-            GoRoute(path: Products.routeName, builder: (context, state) => Products()),
+            /* Products route **/
+            GoRoute(path: Products.routeName, builder: (context, state) => Products(),
+              routes: [
+                GoRoute(path: ProductGroupGridItems.routeName, builder: (context, state) {
+                  Map<String, dynamic> param = state.extra as Map<String, dynamic>;
+                  return ProductGroupGridItems( title: param["title"]!, url: param["url"],);},),
+                GoRoute(path: ProductDetail.routeName, builder: (context, state) {
+                  ProductArguments arguments = state.extra as ProductArguments;
+                  return ProductDetail(id: arguments.id, tag: arguments.tag, merk: arguments.merk, category: arguments.category, 
+                    weight: arguments.weight, price: arguments.price, imageUrl: arguments.imageUrl,);
+                },)
+              ]
+            ),
+            /* Cart route **/
             GoRoute(path: Cart.routeName, builder: (context, state) => Cart()),
+            /* Profile route **/
             GoRoute(path: MyAccount.routeName, builder: (context, state) => MyAccount())
           ]
         )
