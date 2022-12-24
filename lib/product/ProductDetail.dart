abc0f38a1de5e7ab25e9b02edfc0838de24df50d
Ramdan Ariadi <ramadanariadi@gmail.com>
2022-12-24 20:55:46 +0800

    update product detail counter state

--- lib/product/ProductDetail.dart
+++ lib/product/ProductDetail.dart
@@ -1,15 +1,17 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:go_router/go_router.dart';
 import 'package:grocery/constants/Application.dart';
 import 'package:grocery/constants/ApplicationColor.dart';
 import 'package:grocery/custom_widget/Button.dart';
 import 'package:grocery/home/Home.dart';
-import 'package:http/http.dart' as http;
+import 'package:grocery/services/HttpRequestService.dart';
+import 'package:grocery/state_manager/CounterState.dart';
 
 // ignore: must_be_immutable
 class ProductDetail extends StatefulWidget {
-  static final routeName = '/detailProduct';
+  static final routeName = 'detailProduct';
 
   final String id;
   final String tag;
@@ -42,6 +44,8 @@ class _ProductDetail extends State<ProductDetail> {
   Icon loveIcon = Icon(Icons.favorite_outline, size: 30);
   bool productLoved = false;
   bool widgetExist = true;
+  String userId = "ac723ce6-11d2-11ec-82a8-0242ac130003";
+  CounterState _counterState = CounterState();
 
   _ProductDetail();
 
@@ -55,11 +59,9 @@ class _ProductDetail extends State<ProductDetail> {
   Future<void> like() async {
     final response;
     if (productLoved) {
-      response = await http.delete(Uri.parse(Application.httBaseUrl +
-          '/wishlist/ac723ce6-11d2-11ec-82a8-0242ac130003/${widget.id}'));
+      response = await HttpRequestService.sendRequest(method: HttpMethod.DELETE, url: Application.httBaseUrl + '/wishlist/$userId/${widget.id}');
     } else {
-      response = await http.post(Uri.parse(Application.httBaseUrl +
-          '/wishlist/ac723ce6-11d2-11ec-82a8-0242ac130003/${widget.id}'));
+      response = await HttpRequestService.sendRequest(method: HttpMethod.POST, url: Application.httBaseUrl + '/wishlist/$userId/${widget.id}');
     }
     if (response.statusCode == 200 && widgetExist) {
       Map<String, dynamic> responseBody = jsonDecode(response.body);
@@ -79,13 +81,12 @@ class _ProductDetail extends State<ProductDetail> {
   }
 
   Future<void> addToCart() async {
-    final response = await http.post(Uri.parse(Application.httBaseUrl +
-        '/cart/ac723ce6-11d2-11ec-82a8-0242ac130003/${widget.id}/$_count'));
+    final response = await HttpRequestService.sendRequest(method: HttpMethod.POST, url: Application.httBaseUrl + '/cart/$userId/${widget.id}/$_count');
     if (response.statusCode == 200 && widgetExist) {
       Map<String, dynamic> responseBody = jsonDecode(response.body);
       if (responseBody['metaData']['code'] == 201) {
         // Fluttertoast.showToast(msg: "yeay product added");
-        Navigator.pushNamed(context, Home.routeName);
+        GoRouter.of(context).go(Home.routeName);
       }
     } else {
       Fluttertoast.showToast(msg: "opps something wrong");
@@ -94,8 +95,7 @@ class _ProductDetail extends State<ProductDetail> {
   }
 
   Future<void> isLiked() async {
-    final response = await http.get(Uri.parse(Application.httBaseUrl +
-        '/wishlist/ac723ce6-11d2-11ec-82a8-0242ac130003/${widget.id}'));
+    final response = await HttpRequestService.sendRequest(method: HttpMethod.GET, url: Application.httBaseUrl + '/wishlist/$userId/${widget.id}');
     if (response.statusCode == 200 && widgetExist) {
       Map<String, dynamic> responseBody = jsonDecode(response.body);
       // Fluttertoast.showToast(msg: "loved", toastLength: Toast.LENGTH_LONG);
@@ -118,146 +118,155 @@ class _ProductDetail extends State<ProductDetail> {
   void dispose() {
     super.dispose();
     widgetExist = false;
+    _counterState.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Container(
-        height: size.height,
-        padding: EdgeInsets.only(
-            top: Application.defaultPadding * 1.4,
-            right: Application.defaultPadding,
-            bottom: Application.defaultPadding,
-            left: Application.defaultPadding),
-        color: ApplicationColor.naturalWhite,
-        child: Stack(children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: size.height,
+          padding: EdgeInsets.only(
+              top: Application.defaultPadding * 1.4,
+              right: Application.defaultPadding,
+              bottom: Application.defaultPadding,
+              left: Application.defaultPadding),
+          child: Stack(children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            GoRouter.of(context).pop();
+                          },
+                          child: Icon(Icons.arrow_back_ios_new, size: 32)),
+                      GestureDetector(
                         onTap: () {
-                          Navigator.pop(context);
+                          // Fluttertoast.showToast(
+                          //     msg: 'loved it ${widget.merk}',
+                          //     toastLength: Toast.LENGTH_LONG);
                         },
-                        child: Icon(Icons.arrow_back_ios_new, size: 32)),
-                    GestureDetector(
-                      onTap: () {
-                        // Fluttertoast.showToast(
-                        //     msg: 'loved it ${widget.merk}',
-                        //     toastLength: Toast.LENGTH_LONG);
-                      },
-                      child: GestureDetector(
-                        child: Container(
-                            margin: EdgeInsets.only(right: 8), child: loveIcon),
-                        onTap: () {
-                          this.like();
-                        },
+                        child: GestureDetector(
+                          child: Container(
+                              margin: EdgeInsets.only(right: 8), child: loveIcon),
+                          onTap: () {
+                            this.like();
+                          },
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 30),
-                  child: Text(
-                    widget.merk,
-                    style: TextStyle(
-                        color: ApplicationColor.textColor,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold),
+                    ],
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 10, bottom: 30),
-                  child: Text(
-                    widget.category,
-                    style: TextStyle(
-                        color: ApplicationColor.shadowColor,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w200),
-                  ),
-                ),
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: size.width * 0.7),
-                  child: Hero(
-                      tag: widget.tag,
-                      child: widget.imageUrl == null
-                          ? Image.asset('images/notFound.png')
-                          : Image.network(widget.imageUrl!)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 28),
-                  child: RichText(
-                    text: TextSpan(children: [
-                      TextSpan(
-                          text: "\$${widget.price}",
-                          style: TextStyle(
-                              height: 2,
-                              fontSize: 24,
-                              color: ApplicationColor.blackHint,
-                              fontWeight: FontWeight.bold)),
-                      TextSpan(
-                          text: " /kg",
-                          style: TextStyle(
-                              height: 2, fontSize: 24, color: ApplicationColor.blackHint)),
-                    ]),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    InkWell(
-                        onTap: () {
-                          handleCountChange('minus');
-                        },
-                        child: Container(
-                            width: 36,
-                            height: 36,
-                            decoration: BoxDecoration(
-                                color: ApplicationColor.shadowColor.withOpacity(0.1),
-                                borderRadius: BorderRadius.circular(8)),
-                            child: Icon(Icons.remove, size: 30))),
-                    SizedBox(
-                      width: 25,
-                    ),
-                    Text(
-                      "$_count",
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Text(
+                      widget.merk,
                       style: TextStyle(
-                          fontSize: 24,
-                          color: ApplicationColor.blackHint,
+                          color: ApplicationColor.textColor,
+                          fontSize: 28,
                           fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(
-                      width: 25,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10, bottom: 30),
+                    child: Text(
+                      widget.category,
+                      style: TextStyle(
+                          color: ApplicationColor.shadowColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w200),
                     ),
-                    InkWell(
-                        onTap: () {
-                          handleCountChange('plus');
-                        },
-                        child: Container(
-                            width: 36,
-                            height: 36,
-                            decoration: BoxDecoration(
-                                color: ApplicationColor.shadowColor.withOpacity(0.1),
-                                borderRadius: BorderRadius.circular(8)),
-                            child: Icon(
-                              Icons.add,
-                              size: 30,
-                            ))),
-                  ],
-                )
-              ],
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: size.width * 0.7),
+                    child: Hero(
+                        tag: widget.tag,
+                        child: widget.imageUrl == null
+                            ? Image.asset('images/notFound.png')
+                            : Image.network(widget.imageUrl!)),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 28),
+                    child: RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                            text: "\$${widget.price}",
+                            style: TextStyle(
+                                height: 2,
+                                fontSize: 24,
+                                color: ApplicationColor.blackHint,
+                                fontWeight: FontWeight.bold)),
+                        TextSpan(
+                            text: " /kg",
+                            style: TextStyle(
+                                height: 2, fontSize: 24, color: ApplicationColor.blackHint)),
+                      ]),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      InkWell(
+                          onTap: () {
+                            // handleCountChange('minus');
+                            _counterState.eventSink.add(CounterAction.decreament);
+                          },
+                          child: Container(
+                              width: 36,
+                              height: 36,
+                              decoration: BoxDecoration(
+                                  color: ApplicationColor.shadowColor.withOpacity(0.1),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Icon(Icons.remove, size: 30))),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      StreamBuilder(
+                        stream: _counterState.stateStream,
+                        initialData: 0,
+                        builder: (context, snapshot) {
+                        return Text(
+                          '${snapshot.data}',
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: ApplicationColor.blackHint,
+                              fontWeight: FontWeight.bold),
+                        ); 
+                      },),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      InkWell(
+                          onTap: () {
+                            // handleCountChange('plus');
+                            _counterState.eventSink.add(CounterAction.increament);
+                          },
+                          child: Container(
+                              width: 36,
+                              height: 36,
+                              decoration: BoxDecoration(
+                                  color: ApplicationColor.shadowColor.withOpacity(0.1),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Icon(
+                                Icons.add,
+                                size: 30,
+                              ))),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            child: Center(
-              child: Button(
+            Positioned(
+              bottom: 0,
+              child: Center(
+                child: Button(
+                  color: ApplicationColor.primaryColor,
                   margin: EdgeInsets.all(0),
                   child: Text(
                     'Add to cart',
@@ -270,10 +279,12 @@ class _ProductDetail extends State<ProductDetail> {
                   height: size.height / 11,
                   onTap: () {
                     this.addToCart();
-                  }),
-            ),
-          )
-        ]),
+                  }
+                ),
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
