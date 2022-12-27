54eed520b29064f9344f890069cd77d0136e73f4
Ramdan Ariadi <ramadanariadi@gmail.com>
2022-12-27 18:04:31 +0800

    update allert

--- lib/alert/Alert.dart
+++ lib/alert/Alert.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:grocery/constants/Application.dart';
+import 'package:grocery/constants/ApplicationColor.dart';
 import 'package:grocery/custom_widget/Button.dart';
 
 enum Alerts { success }
 
 class Alert extends StatelessWidget {
+
+  static final routeName = "/alert";
+
   final String message;
   final Alerts icon;
-  final VoidCallback callback;
-  const Alert(
-      {required this.icon, required this.message, required this.callback});
+  final String routeToClose;
+  late Image iconImage;
+  Alert({required this.icon, required this.message, required this.routeToClose}){
+    switch(icon){
+      case Alerts.success:
+        iconImage = Image.asset("images/icons/success.png");
+      break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,21 +29,24 @@ class Alert extends StatelessWidget {
       body: Stack(
         children: [
           Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("images/icons/success.png"),
-                Container(
-                  margin: EdgeInsets.only(top: 28),
-                  child: Text(
-                    this.message,
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromRGBO(67, 67, 67, 1)),
-                  ),
-                )
-              ],
+            child: Padding(
+              padding: EdgeInsets.only(bottom: Application.defaultPadding * 8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  iconImage,
+                  Container(
+                    margin: EdgeInsets.only(top: 28),
+                    child: Text(
+                      this.message,
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(67, 67, 67, 1)),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Positioned(
@@ -40,6 +54,7 @@ class Alert extends StatelessWidget {
             child: Center(
               child: Button(
                   margin: EdgeInsets.all(Application.defaultPadding),
+                  color: ApplicationColor.primaryColor,
                   child: Text(
                     'OK',
                     style: TextStyle(
@@ -49,7 +64,9 @@ class Alert extends StatelessWidget {
                   ),
                   width: size.width - (Application.defaultPadding * 2),
                   height: size.height / 11,
-                  onTap: callback),
+                  onTap: (){
+                    GoRouter.of(context).go(routeToClose);
+                  }),
             ),
           )
         ],
