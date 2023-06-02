d2b92d205c910334328dfe692a5ac9db6121ea8f
Ramdan Ariadi <ramdan.ariadi@barrans.id>
2023-06-02 14:45:13 +0800

    refactor state manager

--- lib/state_manager/CounterState.dart
+++ lib/state_manager/CounterState.dart
@@ -1,34 +1,34 @@
-
 import 'dart:async';
 
-enum CounterAction{increament, decreament}
+enum CounterAction { increament, decreament }
 
-class CounterState{
+class CounterState {
   int _counter = 0;
   late bool unsigned;
-  
-  StreamController<CounterAction> _eventController = StreamController<CounterAction>();
+
+  StreamController<CounterAction> _eventController =
+      StreamController<CounterAction>();
   StreamSink<CounterAction> get eventSink => _eventController.sink;
 
   StreamController<int> _stateController = StreamController<int>();
   StreamSink<int> get _stateSink => _stateController.sink;
   Stream<int> get stateStream => _stateController.stream;
 
-  void _mapEventToState(CounterAction counterAction){
-    if(counterAction == CounterAction.increament)
+  void _mapEventToState(CounterAction counterAction) {
+    if (counterAction == CounterAction.increament)
       _counter++;
     else
       _counter = _counter != 0 || unsigned ? --_counter : _counter;
-      
+
     _stateSink.add(_counter);
   }
 
-  CounterState({bool? unsigned}){
+  CounterState({bool? unsigned}) {
     this.unsigned = unsigned ?? false;
     _eventController.stream.listen(_mapEventToState);
   }
 
-  void dispose(){
+  void dispose() {
     _eventController.close();
     _stateController.close();
   }
