57f89b6dc4320844063dfadf98505172346c35ce
ramdan ariadi <ramadanariadi@gmail.com>
2023-06-08 18:11:04 +0800

    refactor custom search bar

--- lib/search_page/CustomSearchBar.dart
+++ lib/search_page/CustomSearchBar.dart
@@ -4,9 +4,10 @@ class CustomSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Custom search"),actions: [
-        IconButton(onPressed: (){}, icon: Icon(Icons.search))
-      ],),
+      appBar: AppBar(
+        title: Text("Custom search"),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+      ),
     );
   }
 }
