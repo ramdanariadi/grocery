b793e37a48d46682c3fb2a0355344d81a29b05ea
ramdan ariadi <ramadanariadi@gmail.com>
2023-06-14 18:26:10 +0800

    enhance products page lazy load

--- lib/state_manager/PaginationState.dart
+++ lib/state_manager/PaginationState.dart
@@ -5,15 +5,30 @@ class PaginationState extends Bloc<List<dynamic>, List<dynamic>> {
   List<dynamic> data;
   int pageSize;
   int pageIndex;
+  bool _isLoading = false;
 
   PaginationState({required this.data, this.pageIndex = 0, this.pageSize = 10})
       : super(data) {
     on<List<dynamic>>((event, emit) {
-      this.data.addAll(event);
       emit(List.of(this.data));
       debugPrint(
           "scroll controller fetch product emited " + event.length.toString());
     });
+  }
+
+  void setIsLoading(bool isLoading, Widget? widget) {
+    this._isLoading = isLoading;
+    if (this._isLoading) {
+      this.data.add(widget);
+      this.add(this.data);
+    } else {
+      this.data.removeLast();
+      this.add(this.data);
+    }
+  }
+
+  bool isLoading() {
+    return this._isLoading;
   }
 
   void refresh({required List<dynamic> data, int? pageSize, int? pageIndex}) {
@@ -25,6 +40,12 @@ class PaginationState extends Bloc<List<dynamic>, List<dynamic>> {
       this.pageSize = pageSize;
     }
 
-    this.add(data);
+    if (this._isLoading) {
+      this.data.removeLast();
+      this._isLoading = false;
+    }
+
+    this.data.addAll(data);
+    this.add(this.data);
   }
 }
